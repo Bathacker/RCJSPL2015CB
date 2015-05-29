7ed5a92b70281e799a3e8deddce01f85671a64f7
Python <diegorscorpion@gmail.com>
2015-05-29 18:05:38 -0500

    :)

--- VNH5019/Omnidireccional.h
+++ VNH5019/Omnidireccional.h
@@ -8,39 +8,43 @@ class Ominidireccional
 	public:
 
 		Ominidireccional();
-		adelante(int vel);
-		adelante();
-		atras(int vel);
-		atras();
-		izquierda(int vel);
-		izquierda();
-		derecha(int vel);
-		derecha();
-		eSDer(int vel);
-		eSDer();
-		eSIzq(int vel);
-		eSIzq();
-		eIIzq(int vel);
-		eIIzq();
-		eIDer(int vel);
-		eIDer();
-		alto();
-		Motor1adel(int vel);
-		Motor1atras(int vel);
-		Motor1alto();
-		Motor2adel(int vel);
-		Motor2atras(int vel);
-		Motor2alto();
-		Motor3adel(int vel);
-		Motor3atras(int vel);
-		Motor3alto();
-		Motor4adel(int vel);
-		Motor4atras(int vel);
-		Motor4alto();
+		void setM1puente1(int M1PWM, int M1INB, int M1INA, int M1EN);
+        void setM2puente1(int M2PWM, int M2INB, int M2INA, int M2EN);
+        void setM1puente2(int M1PWM, int M1INB, int M1INA, int M1EN);
+        void setM2puente2(int M2PWM, int M2INB, int M2INA, int M2EN);
+		void adelante(int vel);
+		void adelante();
+		void atras(int vel);
+		void atras();
+		void izquierda(int vel);
+		void izquierda();
+		void derecha(int vel);
+		void derecha();
+		void eSDer(int vel);
+		void eSDer();
+		void eSIzq(int vel);
+		void eSIzq();
+		void eIIzq(int vel);
+		void eIIzq();
+		void eIDer(int vel);
+		void eIDer();
+		void alto();
+		void Motor1adel(int vel);
+		void Motor1atras(int vel);
+		void Motor1alto();
+		void Motor2adel(int vel);
+		void Motor2atras(int vel);
+		void Motor2alto();
+		void Motor3adel(int vel);
+		void Motor3atras(int vel);
+		void Motor3alto();
+		void Motor4adel(int vel);
+		void Motor4atras(int vel);
+		void Motor4alto();
 
 	private:
 
-		VNH5019 diego, yoalli;
+		VNH5019 puente1, puente2;
 
 };
 #endif
