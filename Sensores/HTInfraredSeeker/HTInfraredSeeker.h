fe975ac938bfca74979eeb0d2b99d85243aece3c
Python <diegorscorpion@gmail.com>
2015-06-18 18:39:56 -0500

    :)

--- Sensores/HTInfraredSeeker/HTInfraredSeeker.h
+++ Sensores/HTInfraredSeeker/HTInfraredSeeker.h
@@ -2,7 +2,7 @@
 #define HTInfraredSeeker_h
 #include "Arduino.h"
 	
-	struct InfraredInput
+	struct InfraredInput //Sirve como un arreglo para guardar
 	{
 	
 	  	byte Direction;
@@ -15,13 +15,13 @@
 		
 		public:
 		
-			static void Initialize();
-			static void ReadACRaw(byte* buffer);
-			static InfraredInput ReadAC();
+			static void Initialize(); //Metodo que inicia la comunicacion con el I2C
+			static void ReadACRaw(byte* buffer); //Metodo para leer valores
+			static InfraredInput ReadAC(); //Recibe todos los metodos de lectura
 	  	
 	  	private:
 		
-			static InfraredInput PopulateValues(byte* buffer);
+			static InfraredInput PopulateValues(byte* buffer); //Obtiene la direccion de la pelota y la intensidad
 			static void ReadValues(byte OffsetAddress, byte* buffer);
 			static const int Address = 0x10 / 2; //Divide by two as 8bit-I2C address is provided
 	
