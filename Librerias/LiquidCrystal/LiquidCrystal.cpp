fb55b2e6ddcbe676eb2b612379f3a8f18c949033
Python <diegorscorpion@gmail.com>
2015-06-26 17:40:50 -0500

    :)

--- Librerias/LiquidCrystal/LiquidCrystal.cpp
+++ Librerias/LiquidCrystal/LiquidCrystal.cpp
@@ -202,44 +202,67 @@ void LiquidCrystal::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
 /********** high level commands, for the user! */
 void LiquidCrystal::clear()
 {
+  
   command(LCD_CLEARDISPLAY);  // clear display, set cursor position to zero
   delayMicroseconds(2000);  // this command takes a long time!
+
 }
 
 void LiquidCrystal::home()
 {
+
   command(LCD_RETURNHOME);  // set cursor position to zero
   delayMicroseconds(2000);  // this command takes a long time!
+
 }
 
 void LiquidCrystal::setCursor(uint8_t col, uint8_t row)
 {
+
   int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-  if ( row > _numlines ) {
+  if (row > _numlines)
+  {
+  
     row = _numlines-1;    // we count rows starting w/0
+  
   }
   
   command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
+
 }
 
 // Turn the display on/off (quickly)
-void LiquidCrystal::noDisplay() {
+void LiquidCrystal::noDisplay()
+{
+
   _displaycontrol &= ~LCD_DISPLAYON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
+
 }
-void LiquidCrystal::display() {
+
+void LiquidCrystal::display()
+{
+
   _displaycontrol |= LCD_DISPLAYON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
+
 }
 
 // Turns the underline cursor on/off
-void LiquidCrystal::noCursor() {
+void LiquidCrystal::noCursor()
+{
+
   _displaycontrol &= ~LCD_CURSORON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
+
 }
-void LiquidCrystal::cursor() {
+
+void LiquidCrystal::cursor()
+{
+
   _displaycontrol |= LCD_CURSORON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
+
 }
 
 // Turn on and off the blinking cursor
--- Pruebas/Prueba/Prueba.ino
+++ Pruebas/Prueba/Prueba.ino
@@ -1,20 +1,38 @@
 #include <Wire.h>
+
 #include <HCSR04.h>
+
 #include <MAG3110.h>
+
 #include <TCS3200.h>
-#include <VNH5019.h>
+
+#include <LiquidCrystal.h>
+
 #include <Omnidireccional.h>
+
 #include <HTInfraredSeeker.h>
+
+MAG3110 compas();
+HCSR04 ultrasonico(1, 2);
+TCS3200 color();
+Omnidireccional omni();
+InfraredSeeker ir();
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+int backLight = 13;
 
 void setup()
 {
+
+  pinMode(backLight, OUTPUT);
+  analogWrite(backLight, 150);
+
+  lcd.begin(16, 2);
+  lcd.clear();
+    
+  lcd.setCursor(0, 0);
   
-  MAG3110 compas();
-  HCSR04 ultra(1, 2, 3);
-  TCS3200 color();
-  Omnidireccional omni();
-  InfraredSeeker ir();
-  
+  lcd.print(ultrasonico.imprimirValores());
+ 
 }
 
 void loop()
