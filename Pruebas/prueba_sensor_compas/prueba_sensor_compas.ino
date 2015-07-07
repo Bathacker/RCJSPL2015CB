56f25724ae46d4d8f55260e3bc17b0966bcb9740
yoallii <yoallipaola@gmail.com>
2015-07-07 15:32:44 -0500

    MAG3110
    pruebas sensor de compas

---
+++ Pruebas/prueba_sensor_compas/prueba_sensor_compas.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+
+#include <MAG3110.h>
+MAG3110 sensor_compas=MAG3110();
+int x;
+
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+   sensor_compas.iniciar();   
+}
+
+void loop() {
+
+ sensor_compas.imprimirValores();
+ delay(1500);
+
+
+}
