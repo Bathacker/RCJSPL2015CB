dfc3a8adbe6eca0912bb3a06f8d0b3a95af3470a
yoallii <yoallipaola@gmail.com>
2015-07-10 17:45:31 -0500

    todo junto
    tiene errores pero esta arreglado

--- Pruebas/Delantero_tonto_uno/Delantero_tonto_uno.ino
+++ Pruebas/Delantero_tonto_uno/Delantero_tonto_uno.ino
@@ -1,3 +1,5 @@
+#include <HMC5883L.h>
+
 #include <HCSR04.h>
 
 //Intelirobot escuela de robotica
@@ -13,6 +15,7 @@
 //.............................
 
 //Instanceas de librerias
+HMC5883L compass = HMC5883L();
 HCSR04 us= HCSR04(33, 31, 11900); //Pines sensor ultrasonico //Sensor ultrasonico
 TCS3200 sensor_color;//sensor de color1
 TCS3200 sensor_color2;//sensor de color2
@@ -25,7 +28,7 @@ InfraredInput seekerInput; //estructura InfraredInput
 int verde1, verde2, verde3,cm;
 int ultrasonico;
 byte ir, intensidad;
-//int luz;  //Verde alrededor de 25, negro alrededor de 15, blanco alrededor de 40   
+int luz;  //Verde alrededor de 25, negro alrededor de 15, blanco alrededor de 40   
 
 void setup() {
   //Para leer valores
@@ -41,6 +44,13 @@ void setup() {
   robot.setM2puente1(2,17,16,15); //Pines de motor 2 en puentte h primero
   robot.setM2puente2(3,14,4,5); //Pines de motor 2 en puente h segundo
   robot.setM1puente1(10,11,12,13);//Pines de motor 1 en puentte h primero 
+  
+float headingDegrees,heading;
+int error = 0;
+int MilliGauss_OnThe_XAxis;
+float declinationAngle = 0.0457;
+int c_actual, c_inicio,brujula;
+ c_inicio=obtenerGrados();
   //....................................................
   
   //Encendemos puentes H
@@ -49,8 +59,8 @@ void setup() {
 }
 
 void loop() {
-
-
+   c_actual = obtenerGrados();
+  brujula = arreglarCompas();
 
   //Necesario llamar metodo color de los sensores de color para leer datos
   sensor_color3.color();
@@ -79,6 +89,97 @@ void loop() {
   //Que el robot juegue fut
   followball();
 }
+ //Alineamos el robot si esta chueco.................
+  if(brujula >= 20 && brujula <= 345)
+  {
+      
+    if(brujula >= 183)
+    {
+      robot.alto();
+      
+      do
+      {
+        
+        robot.alinearDer(70);
+        robot.Motor2alto();
+        robot.Motor4alto();
+      
+      }while(brujula > 350);
+      
+    }
+     
+    else
+    {
+       robot.alto();
+       
+       do
+       {          
+         
+         robot.alinearIzq(70);
+         robot.Motor1alto();
+         robot.Motor3alto();
+       
+       }while(brujula < 10);   
+    
+    }
+    
+  }
+         //Si esta acomodado entonces.................................
+  else
+  {
+    
+    robot.alto();
+    
+  }
+
+}
+
+int obtenerGrados()
+{ 
+  
+  MagnetometerRaw raw = compass.ReadRawAxis();
+  MagnetometerScaled scaled = compass.ReadScaledAxis();
+  
+  // Values are accessed like so:
+  MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
+
+  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  heading = atan2(scaled.YAxis, scaled.XAxis);
+  
+  heading += declinationAngle;
+  
+  // Correct for when signs are reversed.
+   if(heading < 0)
+   
+      heading += 2*PI;
+   
+   // Check for wrap due to addition of declination.
+   if(heading > 2*PI)
+   
+      heading -= 2*PI;
+   
+   // Convert radians to degrees for readability.
+   headingDegrees = heading * 180/M_PI; 
+   
+   return headingDegrees;
+
+}
+
+int arreglarCompas()
+{
+    
+    int b;
+    b = c_actual - c_inicio;
+    
+    if(b < 0)
+    {
+    
+      b+=360;
+  
+    }
+    
+    return b;
+}
 
 
 
@@ -97,7 +198,7 @@ void esquinaSupIzq()
   robot.Aadelante(0, 255,255 , 0);
 }
 void esquinaSupDer()
-{
+{   
   robot.Aadelante(255,0,0,255);
 }
 void esquinaInfIzq()
@@ -158,44 +259,133 @@ void followball()
   switch(ir) // switch que sirve como if , de lo contrario de no ver linea blanca hara todos los demas movimientos de los casos
   {
     case 0:
-      robot.alto();
-      esquinaInfDer();
+         if(verde1>15  &&  verde2>15  && verde3>15)
+        {
+         robot.alto();
+         esquinaInfDer();
+       }
+       else
+       {
+      
+         robot.alto();
+        esquinaSupIzq();
+         delay(230);
+       }
       break;
     case 1:
-    robot.alto();
-      esquinaInfDer();
+        if(verde1>15  &&  verde2>15  && verde3>15)
+         {
+          robot.alto();
+          esquinaInfDer();
+        }
+        else
+       {
+         robot.alto();
+        esquinaSupIzq();
+         delay(230);
+     }
       break;
     case 2:
-    robot.alto();
-      izquierda();
-      break;
+         if(verde1>15 && verde2 >15 && verde3>15)
+         {
+         robot.alto();
+         izquierda();
+         }
+        else
+        {
+         robot.alto();
+         derecha();
+         delay(230);
+        }
+        break;
     case 3:
-    robot.alto();
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
+      robot.alto();
        esquinaSupIzq();
+     }
+     else
+     {
+      robot.alto();
+      esquinaInfDer();
+      delay(230);
+     }
        break;
     case 4:
-    robot.alto();
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
+       robot.alto();
        esquinaSupIzq();
+     }
+     else
+     {
+      robot.alto();
+      esquinaInfDer();
+     }
        break;
     case 5:
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
       robot.alto();
       adelante();
+     }
+     else
+     {
+      robot.alto();
+      atras();
+      delay(230);
+     }
       break;
     case 6:
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
     robot.alto();
-    robot.adelante();
+    adelante();
+     }
+     else
+     {
+      robot.alto();
+      atras();
+      delay(230);
+     }
     break;
     case 7:
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
      robot.alto();
      esquinaSupDer();
+     }
+     else
+     {
+      robot.alto();
+      esquinaInfIzq();
+      delay(230);
+     }
      break;
     case 8:
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
      robot.alto();
      derecha();
+     }
+     else
+     {
+      robot.alto();
+      izquierda();
+      delay(230);
+     }
      break;
     case 9:
+     if(verde1>15  &&  verde2>15  && verde3>15)
+     {
      robot.alto();
      esquinaInfDer();
+     }
+     else
+     {
+      robot.alto();
+      esquinaSupIzq();
+     }
      break;
 
 }
