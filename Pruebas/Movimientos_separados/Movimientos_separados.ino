c0ad01218691a301f435439948c10c9f2cc47f8b
yoallii <yoallipaola@gmail.com>
2015-07-10 12:36:08 -0500

    movimientos   separados
    diagonales y adelante

--- Pruebas/Movimientos_separados/Movimientos_separados.ino
+++ Pruebas/Movimientos_separados/Movimientos_separados.ino
@@ -16,7 +16,7 @@ void setup()
   robot.encenderPuente1();
   robot.encenderPuente2();
   delay(1000);
-//robot.Aadelante(100, 255,255 , 110);
+  esquinaInfIzq();
 // robot.eIDer(100,255,255,110);
  delay(800);
   robot.alto();
@@ -31,3 +31,31 @@ void loop()
   
 
 }
+
+//Motor 1 es el parametro 1
+//Motor 2 es el parametro 2
+//Motor 3 es el parametro 4
+//Motor 4 es el parametro 3
+void adelante()
+{
+  robot.Aadelante(100, 255,255 , 120);
+}
+
+void esquinaSupIzq()
+{
+  robot.Aadelante(0, 255,255 , 0);
+}
+void esquinaSupDer()
+{
+  robot.Aadelante(255,0,0,255);
+}
+void esquinaInfIzq()
+{
+  robot.Aatras(255,0,0,255);
+}
+void esquinaInfDer()
+{
+  robot.Aatras(0,255,255,0);
+}
+
+
