55e2b980bbdcc8d6501d6048934d2624c0340d97
gfdgeras <gfdgeras@gmail.com>
2016-12-23 16:43:59 -0600

    Compas
    Compas

--- Librerias/HMC5883L/HMC5883L_Example/HMC5883L_Example.ino
+++ Librerias/HMC5883L/HMC5883L_Example/HMC5883L_Example.ino
@@ -25,13 +25,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 HMC5883L compass;
 // Record any errors that may occur in the compass.
 int error = 0;
-
+int l;
 // Out setup routine, here we will configure the microcontroller and compass.
 void setup()
 {
   // Initialize the serial port.
   Serial.begin(9600);
-
+  pinMode(2,OUTPUT);
   Serial.println("Starting the I2C interface.");
   Wire.begin(); // Start the I2C interface.
 
@@ -39,7 +39,7 @@ void setup()
   compass = HMC5883L(); // Construct a new HMC5883 compass.
     
   Serial.println("Setting scale to +/- 1.3 Ga");
-  error = compass.SetScale(1.3); // Set the scale of the compass.
+  error = compass.SetScale(5.6); // Set the scale of the compass.
   if(error != 0) // If there is an error, print it out.
     Serial.println(compass.GetErrorText(error));
   
@@ -52,6 +52,8 @@ void setup()
 // Our main program loop.
 void loop()
 {
+   l=digitalRead(2);
+  
   // Retrive the raw values from the compass (not scaled).
   MagnetometerRaw raw = compass.ReadRawAxis();
   // Retrived the scaled values from the compass (scaled to the configured scale).
@@ -60,29 +62,9 @@ void loop()
   // Values are accessed like so:
   int MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
 
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  float heading = atan2(scaled.YAxis, scaled.XAxis);
-  
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  // Find yours here: http://www.magnetic-declination.com/
-  // Mine is: 2� 37' W, which is 2.617 Degrees, or (which we need) 0.0456752665 radians, I will use 0.0457
-  // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
-  float declinationAngle = 0.0457;
-  heading += declinationAngle;
-  
-  // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
-    
-  // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
 
   // Output the data via the serial port.
-  Output(raw, scaled, heading, headingDegrees);
+  Output(raw, scaled);
 
   // Normally we would delay the application by 66ms to allow the loop
   // to run at 15Hz (default bandwidth for the HMC5883L).
@@ -92,7 +74,7 @@ void loop()
 }
 
 // Output the data down the serial port.
-void Output(MagnetometerRaw raw, MagnetometerScaled scaled, float heading, float headingDegrees)
+void Output(MagnetometerRaw raw, MagnetometerScaled scaled)
 {
    Serial.print("Raw:\t");
    Serial.print(raw.XAxis);
@@ -106,11 +88,9 @@ void Output(MagnetometerRaw raw, MagnetometerScaled scaled, float heading, float
    Serial.print("   ");   
    Serial.print(scaled.YAxis);
    Serial.print("   ");   
-   Serial.print(scaled.ZAxis);
+   Serial.println(scaled.ZAxis);
 
-   Serial.print("   \tHeading:\t");
-   Serial.print(heading);
-   Serial.print(" Radians   \t");
-   Serial.print(headingDegrees);
-   Serial.println(" Degrees   \t");
+   
+
+  
 }
--- Pruebas/Prueba_compas/Prueba_compas.ino
+++ Pruebas/Prueba_compas/Prueba_compas.ino
@@ -1,65 +1,130 @@
-#include <Wire.h>
+/*
+HMC5883L_Example.pde - Example sketch for integration with an HMC5883L triple axis magnetomerwe.
+Copyright (C) 2011 Love Electronics (loveelectronics.co.uk)
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the version 3 GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// Reference the I2C Library
+#include <Wire.h>
+// Reference the HMC5883L Compass Library
 #include <HMC5883L.h>
 
-HMC5883L compass = HMC5883L();
-float headingDegrees,heading;
-int error=0;
-int MilliGauss_OnThe_XAxis; 
+// Store our compass as a variable.
+HMC5883L compass;
+// Record any errors that may occur in the compass.
+int error = 0;
+
+int rX=0,rY=0, rX_actual=0, rY_actual=0;
+int x,y;
+// Out setup routine, here we will configure the microcontroller and compass.
+void setup()
+{
+  // Initialize the serial port.
+  Serial.begin(9600);
+
+  Serial.println("Starting the I2C interface.");
+  Wire.begin(); // Start the I2C interface.
+
+  Serial.println("Constructing new HMC5883L");
+  compass = HMC5883L(); // Construct a new HMC5883 compass.
+    
+  Serial.println("Setting scale to +/- 1.7 Ga");
+  error = compass.SetScale(1.3); // Set the scale of the compass.
+  if(error != 0) // If there is an error, print it out.
+    Serial.println(compass.GetErrorText(error));
+  
+  Serial.println("Setting measurement mode to continous.");
+  error = compass.SetMeasurementMode(Measurement_Continuous); // Set the measurement mode to Continuous
+  if(error != 0) // If there is an error, print it out.
+    Serial.println(compass.GetErrorText(error));
+  
+   // Retrive the raw values from the compass (not scaled).
+  MagnetometerRaw raw = compass.ReadRawAxis();
+  // Retrived the scaled values from the compass (scaled to the configured scale).
+  MagnetometerScaled scaled = compass.ReadScaledAxis();
+  
+  // Values are accessed like so:
+  int MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
+
+  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  float heading = atan2(scaled.YAxis, scaled.XAxis);
+  
+  rX=raw.XAxis;
+  rY=scaled.ZAxis;
+}
+
+// Our main program loop.
+void loop()
+{
+  // Retrive the raw values from the compass (not scaled).
+  MagnetometerRaw raw = compass.ReadRawAxis();
+  // Retrived the scaled values from the compass (scaled to the configured scale).
+  MagnetometerScaled scaled = compass.ReadScaledAxis();
+  
+  // Values are accessed like so:
+  int MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
+
+  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  float heading = atan2(scaled.YAxis, scaled.XAxis);
+  
   // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
   // Find yours here: http://www.magnetic-declination.com/
   // Mine is: 2� 37' W, which is 2.617 Degrees, or (which we need) 0.0456752665 radians, I will use 0.0457
   // If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
   float declinationAngle = 0.0457;
-  
-void setup()
-{
-  
-  Wire.begin();
-  Serial.begin(9600);
-
-  error = compass.SetScale(1.3); // Set the scale of the compass.
-  if(error != 0) // If there is an error, print it out.
-    Serial.println(compass.GetErrorText(error));
-  
-    Serial.println("Setting measurement mode to continous.");
-  error = compass.SetMeasurementMode(Measurement_Continuous); // Set the measurement mode to Continuous
-  if(error != 0) // If there is an error, print it out.
-    Serial.println(compass.GetErrorText(error));
-  
-}
-
-void loop()
-{
-
-  alinearse();  
-  
-}
-
-void alinearse()
-{
-  MagnetometerRaw raw = compass.ReadRawAxis();
-   MagnetometerScaled scaled = compass.ReadScaledAxis();
-  
-  // Values are accessed like so:
-  MilliGauss_OnThe_XAxis = scaled.XAxis;// (or YAxis, or ZAxis)
-
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  heading = atan2(scaled.YAxis, scaled.XAxis);
-  
-
   heading += declinationAngle;
   
   // Correct for when signs are reversed.
   if(heading < 0)
     heading += 2*PI;
+    
   // Check for wrap due to addition of declination.
   if(heading > 2*PI)
     heading -= 2*PI;
+   
   // Convert radians to degrees for readability.
-  headingDegrees = heading * 180/M_PI; 
+  float headingDegrees = heading * 180/M_PI; 
+  
+  rX_actual=raw.XAxis;
+  rY_actual=scaled.ZAxis;
+  x=resetearX();
+  y=resetearY();
+  
+   Serial.print(" X: ");
+   Serial.print(x);
+   Serial.print("  Y: ");   
+   Serial.println(y);
  
-   Serial.println(" Grados   \t");
-   Serial.print(headingDegrees);
 
+  
 }
+
+
+
+int resetearX()
+{
+    int b;
+    b = rX - rX_actual;   
+    return b;
+}
+
+int resetearY()
+{
+     int b;
+    b = rY_actual - rY;   
+    return b;
+}
+
+
